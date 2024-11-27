e739789b2f5155bb3b44a06f4db1d0eff9c9168d
Lucas Pontes <lucaspontessantana@gmail.com>
2024-11-27 16:48:37 -0300

    featue(Each): Fazendo as views de busca de block e transaction

--- BitcoinBlockExplorer/src/screens/EachBlock/index.tsx
+++ BitcoinBlockExplorer/src/screens/EachBlock/index.tsx
@@ -1,13 +1,12 @@
 import {
-  Button,
-  RefreshControl,
   ScrollView,
   StyleSheet,
   Text,
+  Image,
   View,
 } from "react-native";
 import { useRoute } from "@react-navigation/native";
-import { useCallback, useState, useEffect } from "react";
+import {  useEffect } from "react";
 import { useFetchData } from "../../../hooks/hooks";
 import LoadingView from "../../components/Loading";
 import {
@@ -15,8 +14,7 @@ import {
   Transactions,
 } from "../../../services/TransactionsBlockRequest";
 import { RouteTypes } from "../../routes/stack";
-import { randomNumber } from "../../components/Generals";
-import Colors from "../../components/Colors";
+import { convertDate, randomNumber } from "../../components/Generals";
 
 const EachBlock = () => {
   const route = useRoute<RouteTypes>();
@@ -58,47 +56,193 @@ const EachBlock = () => {
   return (
     <ScrollView>
       <View style={styles.container}>
-        <Text>Block: {height}</Text>
-        <Text>Hash: {hashBlock}</Text>
-        <Text>Date: {date}</Text>
-        <Text>Size: {size}</Text>
-        <Text>Median Fee: {medianFee}</Text>
-        <Text>Miner: {miner}</Text>
+        <View>
+          <Text
+            style={{
+              padding: 15,
+              borderRadius: 12,
+              backgroundColor: "lightgray",
+              marginHorizontal: 12,
+              marginTop: 20,
+            }}
+          >
+            Block: {height}
+          </Text>
+        </View>
 
-        <Text>{numberTransactions} transactions</Text>
+        <Text
+          style={{
+            padding: 15,
+            borderRadius: 12,
+            backgroundColor: "lightgray",
+            marginHorizontal: 12,
+            marginTop: 20,
+          }}
+          numberOfLines={1}
+        >
+          Hash: {hashBlock}
+        </Text>
 
-        {data.map((transactionsBlock) => (
-          <View key={randomNumber()}>
-            <Text numberOfLines={1} style={{ width: 300 }}>
-              {transactionsBlock.txid}
-            </Text>
-            <Text>Date: {transactionsBlock.status.block_time}</Text>
-
-            {/* transacoes de entrada, lado esquerdo antes da seta no app oficial */}
-            {transactionsBlock.vin.map((vin) => (
-              <View key={randomNumber()}>
-                <Text style={{ color: "blue" }}>
-                  {vin.prevout?.scriptpubkey_address}
-                </Text>
-                <Text style={{ color: "red" }}>
-                  {(vin.prevout?.value ?? 0) / 100000000}
-                </Text>
-              </View>
-            ))}
-
-            {/* transacoes de saida, lado direito depois da seta no app oficial */}
-            {transactionsBlock.vout.map((vout) => (
-              <View key={randomNumber()}>
-                <Text style={{ color: "green" }}>
-                  {vout.scriptpubkey_address}
-                </Text>
-                <Text style={{ color: "orange" }}>
-                  {(vout.value ?? 0) / 100000000}
-                </Text>
-              </View>
-            ))}
+        <View
+          style={{
+            justifyContent: "space-between",
+            flexDirection: "column",
+            borderRadius: 15,
+            backgroundColor: "lightgray",
+            marginHorizontal: 12,
+            marginTop: 20,
+          }}
+        >
+          <View
+            style={{
+              justifyContent: "space-between",
+              flexDirection: "row",
+              padding: 15,
+              width: "100%",
+            }}
+          >
+            <Text>Date:</Text>
+            <Text>{convertDate(date)}</Text>
           </View>
-        ))}
+
+          <View
+            style={{
+              justifyContent: "space-between",
+              flexDirection: "row",
+              padding: 15,
+              width: "100%",
+            }}
+          >
+            <Text>Size:</Text>
+            <Text>{(size / 1000000).toFixed(2)} MB</Text>
+          </View>
+
+          <View
+            style={{
+              justifyContent: "space-between",
+              flexDirection: "row",
+              padding: 15,
+              width: "100%",
+            }}
+          >
+            <Text>Median Fee:</Text>
+            <Text>~{medianFee.toFixed(2)} sat/vB</Text>
+          </View>
+
+          <View
+            style={{
+              justifyContent: "space-between",
+              flexDirection: "row",
+              padding: 15,
+              width: "100%",
+            }}
+          >
+            <Text>Miner:</Text>
+            <Text>{miner}</Text>
+          </View>
+        </View>
+
+        <Text
+          style={{
+            marginHorizontal: 12,
+            marginTop: 20,
+          }}
+        >
+          {numberTransactions} transactions
+        </Text>
+
+        <View
+          style={{
+            width: "100%",
+            marginTop: 20,
+          }}
+        >
+          {data.map((transactionsBlock) => (
+            <View key={randomNumber()}>
+              <View
+                style={{
+                  padding: 15,
+                  borderRadius: 12,
+                  backgroundColor: "lightgray",
+                  marginHorizontal: 12,
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Text
+                  style={{
+                    color: "blue",
+                    width: "55%",
+                  }}
+                  numberOfLines={1}
+                >
+                  {transactionsBlock.txid}
+                </Text>
+                <Text>
+                  {transactionsBlock.status.block_time == null
+                    ? "Aguardando Confirmação"
+                    : convertDate(transactionsBlock.status.block_time)}
+                </Text>
+              </View>
+
+              <View
+                style={{
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  flexDirection: "row",
+                  padding: 15,
+                  borderRadius: 15,
+                  backgroundColor: "purple",
+                  marginHorizontal: 12,
+                  marginTop: 10,
+                  marginBottom: 10,
+                }}
+              >
+                <View>
+                  {/* transacoes de entrada, lado esquerdo antes da seta no app oficial */}
+                  {transactionsBlock.vin.map((vin) => (
+                    <View key={randomNumber()}>
+                      <Text
+                        numberOfLines={1}
+                        style={{ width: 100, color: "white" }}
+                      >
+                        {vin.prevout?.scriptpubkey_address}
+                      </Text>
+                      <Text
+                        numberOfLines={1}
+                        style={{ width: 100, color: "white" }}
+                      >
+                        {(vin.prevout?.value ?? 0) / 100000000}
+                      </Text>
+                    </View>
+                  ))}
+                </View>
+
+                <Image source={require("../../../assets/arrow.png")} />
+
+                <View>
+                  {/* transacoes de saida, lado direito depois da seta no app oficial */}
+                  {transactionsBlock.vout.map((vout) => (
+                    <View key={randomNumber()}>
+                      <Text
+                        numberOfLines={1}
+                        style={{ width: 100, color: "white" }}
+                      >
+                        {vout.scriptpubkey_address}
+                      </Text>
+                      <Text
+                        numberOfLines={1}
+                        style={{ width: 100, color: "white" }}
+                      >
+                        {(vout.value ?? 0) / 100000000}
+                      </Text>
+                    </View>
+                  ))}
+                </View>
+              </View>
+            </View>
+          ))}
+        </View>
       </View>
     </ScrollView>
   );
@@ -109,8 +253,8 @@ export default EachBlock;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: Colors.background,
-    alignItems: "center",
+    backgroundColor: "#fff",
+    alignItems: "flex-start",
     justifyContent: "center",
   },
 });
